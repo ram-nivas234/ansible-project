3e035f73e2b06b4d0c698da2b7193e9e58a077c3
ram-nivas234 <ram.talan234@gmail.com>
2025-06-02 12:55:45 +0530

    Add files via upload

---
+++ master.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+set -e
+
+# Disable swap
+echo "-------------Disabling swap-------------"
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# containerd install
+echo "-------------Installing Containerd-------------"
+wget https://github.com/containerd/containerd/releases/download/v2.0.5/containerd-2.0.5-linux-amd64.tar.gz
+tar Cxzvf /usr/local containerd-2.0.5-linux-amd64.tar.gz
+rm -rf containerd-2.0.5-linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+mkdir -p /usr/local/lib/systemd/system/
+mv containerd.service /usr/local/lib/systemd/system/containerd.service
+systemctl daemon-reload
+systemctl enable --now containerd
+echo " containerd installed successfully "
+
+echo "-------------install Runc-------------"
+wget https://github.com/opencontainers/runc/releases/download/v1.3.0-rc.2/runc.amd64
+install -m 755 runc.amd64 /usr/local/sbin/runc
+rm -rf runc.amd64
+
+echo "-------------install Cni-------------"
+wget https://github.com/containernetworking/plugins/releases/download/v1.6.2/cni-plugins-linux-amd64-v1.6.2.tgz
+mkdir -p /opt/cni/bin
+tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.6.2.tgz
+rm -rf cni-plugins-linux-amd64-v1.6.2.tgz
+
+echo "-------------install Crictl-------------"
+VERSION="v1.32.0" # check latest version in /releases page
+wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+rm -f crictl-$VERSION-linux-amd64.tar.gz
+
+cat <<EOF | sudo tee /etc/crictl.yaml
+runtime-endpoint: unix:///run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+timeout: 2
+debug: false
+pull-image-on-create: false
+EOF
+
+# Forwarding IPv4 and letting iptables see bridged traffic
+echo "-------------Setting IPTables-------------"
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+modprobe br_netfilter
+sysctl -p /etc/sysctl.conf
+
+# install kubeadm,kubelet,kubectl
+echo "-------------installing kubeadm, kubelet, kubectl-------------"
+
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
+
+echo "Printing kubeadm , kubectl , kubelet versions"
+kubeadm version
+kubectl version --client
+kubelet --version
+
+echo "-------------Pulling Kueadm Images -------------"
+kubeadm config images pull
+
+echo "-------------Running kubeadm init-------------"
+kubeadm init
+
+echo "-------------Copying Kubeconfig-------------"
+mkdir -p /root/.kube
+cp -i /etc/kubernetes/admin.conf /root/.kube/config
+
+REGULAR_USER="anshul" #Replace ubuntu with your actual regular username if different.
+mkdir -p /home/${REGULAR_USER}/.kube
+sudo cp -i /etc/kubernetes/admin.conf /home/${REGULAR_USER}/.kube/config
+sudo chown ${REGULAR_USER}:${REGULAR_USER} /home/${REGULAR_USER}/.kube/config
+
+
+echo "-------------Deploying Weavenet Pod Networking-------------"
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml
+#use-- kubectl apply -f https://reweave.azurewebsites.net/k8s/v1.29/net.yaml 
+#if you are facing any problem 
+
+echo "-------------Creating file with join command-------------"
+echo `kubeadm token create --print-join-command` > ./join-command.sh
